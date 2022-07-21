847cc5e3220a6e71e9aa98be03dd4c602a06c33b
Mário Lucas <mariolucasdev@gmail.com>
2022-07-20 22:35:17 -0300

    Add Menu Default

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import '../ui/styles/globals.css';
+import type { AppProps } from 'next/app';
+import styles from '../ui/styles/Home.module.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <header>
+        <h1 className={styles['title']}>
+          Treina
+          <span className={styles['title-secound-word']}>Blog</span>
+        </h1>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
